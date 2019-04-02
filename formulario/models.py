e4b69c376097fab5e24ebad1a1ca863aefea03cc
linkhl09 <af.hernandezl@uniandes.edu.co>
2019-04-02 17:11:25 -0500

    Sorry not sorry m8
    Comente todo lo suyo para probar jajajaja

--- formulario/models.py
+++ formulario/models.py
@@ -2,18 +2,16 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
-class Formulario(models.Model):
-
-    comentario = models.TextField(blank = True)
-    materias = ArrayField(models.ForeignKey(Materia, on_delete = models.CASCADE, related_name = 'nombre_materia'))
-    habilidadesBlandas = ArrayField(models.ForeignKey(HabilidadBlanda, on_delete=models.CASCADE, related_name='nombre_habilidad'))
-
-class Materia(models.Model):
-    nombre = models.charField(max_length = 64)
-
-
-
-
-class HabilidadBlanda(models.Model):
-    nombre = models.charField(max_length = 64)
-
+# class Formulario(models.Model):
+#     comentario = models.TextField(blank=True)
+#     materias = ArrayField(models.ForeignKey(Materia, on_delete=models.CASCADE, related_name='nombre_materia'))
+#     habilidadesBlandas = ArrayField(
+#         models.ForeignKey(HabilidadBlanda, on_delete=models.CASCADE, related_name='nombre_habilidad'))
+#
+#
+# class Materia(models.Model):
+#     nombre = models.charField(max_length=64)
+#
+#
+# class HabilidadBlanda(models.Model):
+#     nombre = models.charField(max_length=64)
