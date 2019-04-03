7ba0d299d5c473328e809f98b997df778bf12be2
linkhl09 <af.hernandezl@uniandes.edu.co>
2019-04-03 11:12:05 -0500

    Estaba usando forms en models :)

--- usuario/models.py
+++ usuario/models.py
@@ -1,16 +1,16 @@
 from django import forms
-from formulario import forms
+from django.db import models
 # Create your models here.
-#
+
 class Usuario(models.Model):
-    username = forms.CharField(max_length=20, required=True, help_text='Obligatorio.')
-    nombres = forms.CharField(max_length=30, required=True, help_text='Obligatorio.')
-    apellidos = forms.CharField(max_length=30, required=True, help_text='Obligatorio.')
-    email = forms.EmailField(max_length=254, help_text='Obligatorio. Inserte una dirección de correo valida.')
-    universidad = forms.CharField(max_length=40, required=False, help_text='Opcional.')
-    fechaNacimiento = forms.DateField(widget=forms.widgets.DateInput(format="%d/%m/%Y"))
-    contrasenia = forms.CharField(widget=forms.PasswordInput()
-    formulario = forms.OneToOneField(Formulario, on_delete = models.CASCADE)
+    username = models.CharField(max_length=20, required=True, help_text='Obligatorio.')
+    nombres = models.CharField(max_length=30, required=True, help_text='Obligatorio.')
+    apellidos = models.CharField(max_length=30, required=True, help_text='Obligatorio.')
+    email = models.EmailField(max_length=254, help_text='Obligatorio. Inserte una dirección de correo valida.')
+    universidad = models.CharField(max_length=40, required=False, help_text='Opcional.')
+    fechaNacimiento = models.DateField(widget=forms.widgets.DateInput(format="%d/%m/%Y"))
+    contrasenia = models.CharField(widget=forms.PasswordInput()
+    formulario = models.OneToOneField(Formulario, on_delete = models.CASCADE, primary_key = True)
 
     def ___str___(self):
         return self.username
