dafc83202b23e0246995253917e8e93b90b85f85
linkhl09 <af.hernandezl@uniandes.edu.co>
2019-04-03 11:30:18 -0500

    .

--- usuario/models.py
+++ usuario/models.py
@@ -5,11 +5,11 @@ from formulario.models import Formulario
 
 
 class Usuario(models.Model):
-    username = models.CharField(max_length=20, required=True, help_text='Obligatorio.')
-    nombres = models.CharField(max_length=30, required=True, help_text='Obligatorio.')
-    apellidos = models.CharField(max_length=30, required=True, help_text='Obligatorio.')
+    username = models.CharField(max_length=20, primary_key = True, help_text='Obligatorio.')
+    nombres = models.CharField(max_length=30, help_text='Obligatorio.')
+    apellidos = models.CharField(max_length=30, help_text='Obligatorio.')
     email = models.EmailField(max_length=254, help_text='Obligatorio. Inserte una dirección de correo valida.')
-    universidad = models.CharField(max_length=40, required=False, help_text='Opcional.')
+    universidad = models.CharField(max_length=40, help_text='Opcional.')
     fechaNacimiento = models.DateField(widget=forms.widgets.DateInput(format="%d/%m/%Y"))
     contrasenia = models.CharField(widget=forms.PasswordInput())
     formulario = models.OneToOneField(
