d1a11c4f2698b51e2fb157922f75177aed611281
Majo <mj.ocampov@uniandes.edu.co>
2019-04-03 13:50:24 -0500

    Compila

--- usuario/models.py
+++ usuario/models.py
@@ -5,17 +5,13 @@ from formulario.models import Formulario
 
 
 class Usuario(models.Model):
-    username = models.CharField(max_length=20, primary_key = True, help_text='Obligatorio.')
-    nombres = models.CharField(max_length=30, help_text='Obligatorio.')
-    apellidos = models.CharField(max_length=30, help_text='Obligatorio.')
-    email = models.EmailField(max_length=254, help_text='Obligatorio. Inserte una dirección de correo valida.')
-    universidad = models.CharField(max_length=40, help_text='Opcional.')
-    fechaNacimiento = models.DateField()
-    contrasenia = models.CharField(max_length=250)
-    formulario = models.OneToOneField(
-        Formulario ,
-        on_delete = models.CASCADE
-    )
+    username = models.CharField(max_length=20, primary_key = True)
+    nombres = models.CharField(max_length=30, default = ' ')
+    apellidos = models.CharField(max_length=30, default = ' ')
+    email = models.EmailField(max_length=254, default = ' ')
+    universidad = models.CharField(max_length=40, default = ' ')
+    fechaNacimiento = models.DateField(null = True)
+    contrasenia = models.CharField(max_length=250, default = ' ')
 
     def ___str___(self):
         return self.username
