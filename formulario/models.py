23ebf38dbfe23171e7e1292f4bea931530c024ff
SantiagoAV <42415733+SantiagoAV@users.noreply.github.com>
2019-04-02 23:31:17 -0500

    espero que funcione

--- formulario/models.py
+++ formulario/models.py
@@ -4,7 +4,8 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 class Formulario(models.Model):
     comentario = models.TextField(blank=True)
-
+    def __str__(self):
+        return self.comentario
 
 
 
@@ -12,11 +13,16 @@ class Materia(models.Model):
     class Meta:
         unique_together =(('nombre','formulario'),)
     nombre = models.CharField(max_length=64)
-    formulario = models.ForeignKey(Formulario,on_delete = models.CASCADE, related_name = 'materia_formulario')
+    formulario = models.ForeignKey(Formulario,on_delete = models.CASCADE, related_name = 'materia_formulario', blank = True )
 
+    def __str__(self):
+        return self.nombre
 
 class HabilidadBlanda(models.Model):
     class Meta:
         unique_together =(('nombre','formulario'),)
     nombre = models.CharField(max_length=64)
-    formulario = models.ForeignKey(Formulario, on_delete=models.CASCADE, related_name='habilidad_formulario')
+    formulario = models.ForeignKey(Formulario, on_delete=models.CASCADE, related_name='habilidad_formulario', blank = True)
+
+    def __str__(self):
+        return self.nombre
