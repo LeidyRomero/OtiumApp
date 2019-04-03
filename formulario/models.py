50a329b65d1a8bad37f5900685ca594a05829e3a
SantiagoAV <42415733+SantiagoAV@users.noreply.github.com>
2019-04-02 20:52:53 -0500

    Revert "frustrante"
    This reverts commit f0d7c138d079d2f71748b7660f6516fcea4359d6.

--- formulario/models.py
+++ formulario/models.py
@@ -1,29 +1,17 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
-
-class Formulario(models.Model):
-    comentario = models.TextField(blank=True)
-    materias = models.TextField["Algorítmica y Programación I", "Algorítmica y Programación II," \
-                                "Matemática Estructural","Fundamentos de Infraestructura Tecnológica"
-                                "Lenguajes y Máquinas", "Estructura de Datos",
-                                "Sistemas Transaccionales", "Desarrollo de SW en Equipo"
-                                "Diseño y Análilsis de Algoritmos", "TI en las Organizaciones",
-                                "Cálculos","Arquitectura Empresarial", "Físicas",
-                                "Infraestructura Computacional", "Arquitectura y Diseño de SW",
-                                "Diseño de Productos e Innovación en TI","Modelado, Simulación y Optimización",
-                                "Sistemas Empresariales","Programación con Tecnologías Web",
-                                "Infraestructura de Comunicaciones", "Inteligencia de Negocios",
-                                "Construcción de Aplicaciones Móviles"]
-    habilidadesBlandas = models.TextField["Adaptabilidad", "Comunicación", "Resolver problemas"
-                                          "Creatividad", "Relaciones interpersonales", "Trabajo en equipo"
-                                          "Paciencia", "Escritura"]
-
-
-
-class Materia(models.Model):
-    nombre = models.charField(max_length=64)
-
-
-class HabilidadBlanda(models.Model):
-    nombre = models.charField(max_length=64)
+# class Formulario(models.Model):
+#     comentario = models.TextField(blank=True)
+#     materias = ArrayField(models.ForeignKey(Materia, on_delete=models.CASCADE, related_name='nombre_materia'))
+#     habilidadesBlandas = ArrayField(
+#         models.ForeignKey(HabilidadBlanda, on_delete=models.CASCADE, related_name='nombre_habilidad'))
+#
+#
+# class Materia(models.Model):
+#     nombre = models.charField(max_length=64)
+#
+#
+# class HabilidadBlanda(models.Model):
+#     nombre = models.charField(max_length=64)
