78ec397180189b9b09c3b1068fe3aa8bf53157b9
linkhl09 <af.hernandezl@uniandes.edu.co>
2019-04-03 11:17:23 -0500

    lo copie literallll

--- usuario/models.py
+++ usuario/models.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.db import models
 # Create your models here.
+from formulario.models import Formulario
+
 
 class Usuario(models.Model):
     username = models.CharField(max_length=20, required=True, help_text='Obligatorio.')
@@ -10,7 +12,11 @@ class Usuario(models.Model):
     universidad = models.CharField(max_length=40, required=False, help_text='Opcional.')
     fechaNacimiento = models.DateField(widget=forms.widgets.DateInput(format="%d/%m/%Y"))
     contrasenia = models.CharField(widget=forms.PasswordInput()
-    formulario = models.OneToOneField(Formulario, on_delete = models.CASCADE, primary_key = True)
+    formulario = models.OneToOneField(
+        Formulario ,
+        on_delete = models.CASCADE ,
+        primary_key = True ,
+    )
 
     def ___str___(self):
         return self.username
