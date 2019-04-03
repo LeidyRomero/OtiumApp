1a80607cf916398156f5dbfc6fbafae17f1bc616
SantiagoAV <42415733+SantiagoAV@users.noreply.github.com>
2019-04-02 23:32:49 -0500

    no funciono lo anterior, intentando de nuevo

--- formulario/models.py
+++ formulario/models.py
@@ -13,7 +13,7 @@ class Materia(models.Model):
     class Meta:
         unique_together =(('nombre','formulario'),)
     nombre = models.CharField(max_length=64)
-    formulario = models.ForeignKey(Formulario,on_delete = models.CASCADE, related_name = 'materia_formulario', blank = True )
+    formulario = models.ForeignKey(Formulario,on_delete = models.CASCADE, related_name = 'materia_formulario', null = True )
 
     def __str__(self):
         return self.nombre
@@ -22,7 +22,7 @@ class HabilidadBlanda(models.Model):
     class Meta:
         unique_together =(('nombre','formulario'),)
     nombre = models.CharField(max_length=64)
-    formulario = models.ForeignKey(Formulario, on_delete=models.CASCADE, related_name='habilidad_formulario', blank = True)
+    formulario = models.ForeignKey(Formulario, on_delete=models.CASCADE, related_name='habilidad_formulario', null = True)
 
     def __str__(self):
         return self.nombre
