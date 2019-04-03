c518f1948aeceb7b613b152a456eb7f59525c527
SantiagoAV <42415733+SantiagoAV@users.noreply.github.com>
2019-04-02 21:15:32 -0500

    cambios en formulario

--- formulario/models.py
+++ formulario/models.py
@@ -4,20 +4,8 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 class Formulario(models.Model):
     comentario = models.TextField(blank=True)
-    materias = models.TextField["Algorítmica y Programación I", "Algorítmica y Programación II," \
-                                "Matemática Estructural","Fundamentos de Infraestructura Tecnológica"
-                                "Lenguajes y Máquinas", "Estructura de Datos",
-                                "Sistemas Transaccionales", "Desarrollo de SW en Equipo"
-                                "Diseño y Análilsis de Algoritmos", "TI en las Organizaciones",
-                                "Cálculos","Arquitectura Empresarial", "Físicas",
-                                "Infraestructura Computacional", "Arquitectura y Diseño de SW",
-                                "Diseño de Productos e Innovación en TI","Modelado, Simulación y Optimización",
-                                "Sistemas Empresariales","Programación con Tecnologías Web",
-                                "Infraestructura de Comunicaciones", "Inteligencia de Negocios",
-                                "Construcción de Aplicaciones Móviles"]
-    habilidadesBlandas = models.TextField["Adaptabilidad", "Comunicación", "Resolver problemas"
-                                          "Creatividad", "Relaciones interpersonales", "Trabajo en equipo"
-                                          "Paciencia", "Escritura"]
+    materias = ArrayField(models.TextField)
+    habilidadesBlandas = ArrayField(models.TextField)
 
 
 
